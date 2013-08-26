e91e9c1b7b5d6dc6dc13b0aadbcb7b7c3b203367
Matt <mattkennedynz@gmail.com>
2013-08-27 08:12:05 +1200

    Re-adds redirectTo route

--- js/config/routes.js
+++ js/config/routes.js
@@ -3,20 +3,26 @@
 
   Commonplace.app.config(['$routeProvider', function($routeProvider) {
 
-    $routeProvider.when( '/:date', {
-      templateUrl: Commonplace.templates.index,
-      controller: 'IndexController'
-    });
+    $routeProvider.
 
-    $routeProvider.when( '/highlights/:highlight_id', {
-      templateUrl: Commonplace.templates.highlights,
-      controller: 'HighlightController'
-    });
+      when( '/:date', {
+        templateUrl: Commonplace.templates.index,
+        controller: 'IndexController'
+      }).
 
-    $routeProvider.when( '/twinglings', {
-      templateUrl: Commonplace.templates.twinglings,
-      controller: 'TwinglingController'
-    });
+      when( '/highlights/:highlight_id', {
+        templateUrl: Commonplace.templates.highlights,
+        controller: 'HighlightController'
+      }).
+
+      when( '/twinglings', {
+        templateUrl: Commonplace.templates.twinglings,
+        controller: 'TwinglingController'
+      }).
+
+      otherwise ({
+        redirectTo: '/'
+      });
 
   }]);
 
